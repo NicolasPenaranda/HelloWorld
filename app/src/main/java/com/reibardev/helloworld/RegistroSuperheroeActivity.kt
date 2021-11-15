bc657948db87c4d2b669284ee516dc6ba3bc399e
NicolasPenaranda <nicolaspenaranda02@gmail.com>
2021-11-14 21:51:15 -0500

    Se crea parcialmente la interfaz de usuario

--- app/src/main/java/com/reibardev/helloworld/RegistroSuperheroeActivity.kt
+++ app/src/main/java/com/reibardev/helloworld/RegistroSuperheroeActivity.kt
@@ -2,9 +2,8 @@ package com.reibardev.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.*
+import com.google.android.material.textfield.TextInputEditText
 
 class RegistroSuperheroeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,10 +13,27 @@ class RegistroSuperheroeActivity : AppCompatActivity() {
         val registrarButton: Button = findViewById(R.id.registrar_button)
         val nombreEditText: EditText = findViewById(R.id.nombre_edit_text)
         val infoTextView: TextView = findViewById(R.id.info_text_view)
+        val estaturaEditText: TextInputEditText = findViewById(R.id.estatura_edit_text)
+        val masculinoRadioButton: RadioButton = findViewById(R.id.masculino_radio_button)
+        val femeninoRadioButton: RadioButton = findViewById(R.id.femenino_radio_button)
+        val fuerzaCheckBox: CheckBox = findViewById(R.id.super_fuerza_check_box)
+        val velocidadCheckBox: CheckBox = findViewById(R.id.velocidad_check_box)
+        val telequinesisCheckBox: CheckBox = findViewById(R.id.telequinesis_check_box)
+        val ciudadNacimientoSpinner: Spinner = findViewById(R.id.ciudad_nacimiento_spinner)
 
         registrarButton.setOnClickListener {
-            val nombre = nombreEditText.text
-            infoTextView.text = nombre
+            val nombre: String = nombreEditText.text.toString()
+            val estatura: Float = estaturaEditText.text.toString().toFloat()
+            var poderes = ""
+            val ciudadNacimiento = ciudadNacimientoSpinner.selectedItem.toString()
+
+            val genero: String = if (masculinoRadioButton.isChecked) getString(R.string.masculino) else getString(R.string.femenino)
+
+            if (fuerzaCheckBox.isChecked) poderes = getString(R.string.super_fuerza)
+            if (velocidadCheckBox.isChecked) poderes = poderes + " " + getString(R.string.velocidad)
+            if (telequinesisCheckBox.isChecked) poderes = poderes + " " + getString(R.string.telequinesis)
+
+            infoTextView.text = getString(R.string.nombre_info, nombre, estatura, genero, poderes, ciudadNacimiento)
         }
     }
 }
