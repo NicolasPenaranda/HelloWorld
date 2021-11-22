58cd043d1552c5f98b67519ba1fcecc683eea0c6
NicolasPenaranda <nicolaspenaranda02@gmail.com>
2021-11-22 09:53:53 -0500

    Creación de un recycler view completo

--- app/src/main/java/com/reibardev/helloworld/SuperHeroesAdapter.kt
+++ app/src/main/java/com/reibardev/helloworld/SuperHeroesAdapter.kt
@@ -7,36 +7,36 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 
 class SuperHeroesAdapter(
-    private val superheroesList: ArrayList<Superheroe>
+    private val superheroesList: ArrayList<SuperheroeItem>
 ) : RecyclerView.Adapter<SuperHeroesAdapter.SuperheroeViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperheroeViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.card_view_superheroe_item, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_view_superheroe_item, parent, false)
         return SuperheroeViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: SuperheroeViewHolder, position: Int) {
         val superheroe = superheroesList[position]
         holder.bind(superheroe)
-
-    }
+        }
 
     override fun getItemCount(): Int = superheroesList.size
 
     class SuperheroeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        private var nameTextView : TextView = itemView.findViewById(R.id.name_text_view)
+        private var nameTextView: TextView = itemView.findViewById(R.id.name_text_view)
         private var aliasTextView: TextView = itemView.findViewById(R.id.alias_text_view)
         private var pictureImageView: ImageView = itemView.findViewById(R.id.picture_image_view)
 
-        fun bind(superheroe: Superheroe) {
+        fun bind(superheroe: SuperheroeItem) {
             Log.d("nombre", superheroe.name)
             nameTextView.text = superheroe.name
             aliasTextView.text = superheroe.alias
-            //picture
+            Picasso.get().load(superheroe.urlPicture).into(pictureImageView)
+
         }
     }
 }
